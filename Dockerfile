d9d932f0f3d16131727420f20123ea9b064526de
Mostafa-wael <mostafa.w.k000@gmail.com>
2022-07-18 17:37:18 +0200

    docker image partially finished

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:lts-alpine
+WORKDIR /usr/src/app
+COPY ["package.json", "package-lock.json*", "lib/", "src/", "./"]
+RUN npm ci 
+COPY . .
+RUN chown -R node /usr/src/app
+
+USER node
+WORKDIR /usr/src/app
+ENTRYPOINT ["node", "src/index.js"]
+CMD ["-d", "0"]
--- lib/utilities/helperFunctions.ts
+++ lib/utilities/helperFunctions.ts
@@ -3,8 +3,7 @@ import colors = require('colors');
 import Table = require('../custom_node_modules/cli-table3');
 import mpv = require('node-mpv');
 
-export function print(str: string, color: string="white") 
-{
+export function print(str: string, color: string = "white") {
     console.log(colors[color](str));
 }
 
@@ -24,22 +23,28 @@ export function showListIndex(arr: string[], header1: string, header2: string, s
 
 export function runFromURL(url: string) {
     // run MPV subprocess
-    let mpvPlayer = new mpv({
-        "verbose": false,
-        "audio_only": true
-    });
-    mpvPlayer.load(url);
-    mpvPlayer.play();
-    mpvPlayer.on('stopped', function () {
-        print("Finished playing", "green");
-        process.exit(1); // close the program
-    });
+    try {
+        let mpvPlayer = new mpv({
+            "verbose": false,
+            "audio_only": true
+        });
+        mpvPlayer.load(url);
+        mpvPlayer.play();
+        mpvPlayer.on('stopped', function () {
+            print("Finished playing", "green");
+            process.exit(1); // close the program
+        });
+    }
+    catch (err) {
+        print("You don't have MPV installed", "red");
+        process.exit(1); // close the program    }
+    }
 }
 
-export function raiseError (name, message) {
+export function raiseError(name, message) {
     const err = new Error(colors.red(message));
     err.name = name;
     // print(err.message, "red");
     //TODO: Raise error and catch it in a higher level
     throw err;
-  }
+}
