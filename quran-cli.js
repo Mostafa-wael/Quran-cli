1a776bbc8b2d0e1309adc128c03d48f01e03aeba
Mostafa-wael <mostafa.w.k000@gmail.com>
2022-07-18 09:30:44 +0200

    handle invalid options

--- quran-cli.js
+++ quran-cli.js
@@ -4,27 +4,43 @@ var parser_1 = require("./utilities/parser");
 var radioMode_1 = require("./modes/radioMode/radioMode");
 var reciterMode_1 = require("./modes/reciterMode/reciterMode");
 var pj = require('./package.json');
-var args = parser_1.CommandLine.getArgs();
-if (args.version == true) {
-    console.log(pj.version);
+try {
+    var args = parser_1.CommandLine.getArgs();
+    if (args.version == true) {
+        console.log(pj.version);
+    }
+    else if (args.radio >= 0) {
+        radioMode_1.runRadio(args.radio);
+    }
+    else if (args.reciterSurah.length > 0) {
+        if (args.reciterSurah.length == 1) {
+            reciterMode_1.showReciterAvailableSuras(args.reciterSurah[0]);
+        }
+        else {
+            reciterMode_1.runSurah(args.reciterSurah[0], args.reciterSurah[1]);
+        }
+    }
+    else if (args.showRadios == true) {
+        radioMode_1.showAllRadios();
+    }
+    else if (args.showReciters == true) {
+        reciterMode_1.showAllReciters();
+    }
+    else if (args.showSuras == true) {
+        reciterMode_1.showReciterAvailableSuras(0);
+    }
 }
-else if (args.radio >= 0) {
-    radioMode_1.runRadio(args.radio);
-}
-else if (args.reciterSurah.length > 0) {
-    if (args.reciterSurah.length == 1) {
-        reciterMode_1.showReciterAvailableSuras(args.reciterSurah[0]);
+catch (err) {
+    if (err.name == "UNKNOWN_OPTION") {
+        console.log("Invalid option. Use -h to see the usage guide.");
+    }
+    else if (err.name == "ALREADY_SET") {
+        console.log("You can't set the same option more than once.");
+    }
+    else if (err.name == "UNKNOWN_VALUE") {
+        console.log("Invalid value. Use -h to see the usage guide.");
     }
     else {
-        reciterMode_1.runSurah(args.reciterSurah[0], args.reciterSurah[1]);
+        console.log(err);
     }
-}
-else if (args.showRadios == true) {
-    radioMode_1.showAllRadios();
-}
-else if (args.showReciters == true) {
-    reciterMode_1.showAllReciters();
-}
-else if (args.showSuras == true) {
-    reciterMode_1.showReciterAvailableSuras(0);
 }
--- quran-cli.ts
+++ quran-cli.ts
@@ -3,29 +3,49 @@ import { runRadio, showAllRadios } from "./modes/radioMode/radioMode";
 import { runSurah, showAllReciters, showReciterAvailableSuras } from "./modes/reciterMode/reciterMode";
 const pj = require('./package.json')
 
-const args: CLIArgs = CommandLine.getArgs();
 
-if(args.version == true) {
-    console.log(pj.version)
+try {
+    const args: CLIArgs = CommandLine.getArgs();
+
+    if (args.version == true) {
+        console.log(pj.version)
+    }
+    else if (args.radio >= 0) {
+        runRadio(args.radio);
+    }
+    else if (args.reciterSurah.length > 0) {
+        if (args.reciterSurah.length == 1) {
+            showReciterAvailableSuras(args.reciterSurah[0]);
+        }
+        else {
+            runSurah(args.reciterSurah[0], args.reciterSurah[1]);
+        }
+    }
+    else if (args.showRadios == true) {
+        showAllRadios();
+    }
+    else if (args.showReciters == true) {
+        showAllReciters();
+    }
+    else if (args.showSuras == true) {
+        // TODO: use the values from the surasDictionary
+        showReciterAvailableSuras(0); // we are sure that the reciter 0 has all the suras.
+    }
 }
-else if (args.radio >= 0) {
-    runRadio(args.radio);
-}
-else if (args.reciterSurah.length > 0) {
-    if (args.reciterSurah.length == 1) {
-        showReciterAvailableSuras(args.reciterSurah[0]);
+catch (err) {
+    // check if the error is a CLI error
+    if (err.name == "UNKNOWN_OPTION") {
+        console.log("Invalid option. Use -h to see the usage guide.");
+    }
+    else if (err.name == "ALREADY_SET")
+    {
+        console.log("You can't set the same option more than once.");
+    }
+    else if(err.name == "UNKNOWN_VALUE")
+    {
+        console.log("Invalid value. Use -h to see the usage guide.");
     }
     else {
-        runSurah(args.reciterSurah[0], args.reciterSurah[1]);
+        console.log(err);
     }
-}
-else if (args.showRadios == true) {
-    showAllRadios();
-}
-else if (args.showReciters == true) {
-    showAllReciters();
-}
-else if (args.showSuras == true) {
-    // TODO: use the values from the surasDictionary
-    showReciterAvailableSuras(0); // we are sure that the reciter 0 has all the suras.
 }
