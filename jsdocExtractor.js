251aea550ded19cb3d777cbc5099b0453318bb01
Mostafa-wael <mostafa.w.k000@gmail.com>
2022-07-18 11:42:08 +0200

    remove js files

--- jsdocExtractor.js
+++
@@ -1,9 +0,0 @@
-var jsdocExtractor = require("jsdoc-extractor");
-var readFileSync = require("fs").readFileSync;
-var buf = readFileSync("./quran-cli.js");
-for (var _i = 0, _a = jsdocExtractor(buf); _i < _a.length; _i++) {
-    var _b = _a[_i], doc = _b[0], start = _b[1], end = _b[2];
-    console.log("Found a new JSDoc block between " + start + " and " + end);
-    console.log(doc.toString());
-    console.log("-------------");
-}
